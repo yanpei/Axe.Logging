34ec9c80fa0b39dfb991fc4bb9e5302305a625af
Blink <blink@thoughtworks.com>
2017-03-26 20:39:43 +0800

    [yanpei] mark exception with log entry

---
+++ src/Axe.Logging.Core/Logging.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Axe.Logging.Core
+{
+    public static class Logging
+    {
+        public static T Mark<T>(this T exception, LogEntry log) where T : Exception
+        {
+            exception.Data.Add("AxeLogging", log);
+            return exception;
+        }
+    }
+
+    [Serializable]
+    public class LogEntry
+    {
+       
+        public LogEntry(DateTime time, string entry, object user, object data, Level level)
+        {
+            Time = time;
+            Entry = entry;
+            User = user;
+            Data = data;
+            Level = level;
+        }
+
+        public DateTime Time { get;}
+        public string Entry { get; }
+        public object User { get; }
+        public object Data { get; }
+        public Level Level { get; }
+    }
+
+
+    public enum Level
+    {
+        DefinedByBusiness,
+        IKnowItWillHappen,
+        Unknown
+    }
+}
---
+++ test/Axe.Logging.Test/LoggingFacts.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics;
+using Axe.Logging.Core;
+using Xunit;
+
+namespace Axe.Logging.Test
+{
+    public class LoggingFacts
+    {
+        [Fact]
+        public void shold_can_mark_log_entry_for_any_exception()
+        {
+            var time = DateTime.UtcNow;
+            var entry = "http://localhost:8080 Post";
+            var user = new {Id = 1};
+            var data = new {Country="China"};
+
+            var logEntryDefinedByBusiness = new LogEntry(time, entry, user, data, Level.DefinedByBusiness);
+            Exception exceptionDefinedByBusiness = new Exception().Mark(logEntryDefinedByBusiness);
+
+            var logEntryIKnowItWillHappen = new LogEntry(time, entry, user, data, Level.IKnowItWillHappen);
+            Exception exceptionIKnowItWillHappen = new Exception().Mark(logEntryIKnowItWillHappen);
+
+            var logEntryUnKnown = new LogEntry(time, entry, user, data, Level.Unknown);
+            Exception exceptionUnKnow = new Exception().Mark(logEntryUnKnown);
+
+            Assert.Equal(exceptionDefinedByBusiness.Data["AxeLogging"], logEntryDefinedByBusiness);
+            Assert.Equal(exceptionIKnowItWillHappen.Data["AxeLogging"], logEntryIKnowItWillHappen);
+            Assert.Equal(exceptionUnKnow.Data["AxeLogging"], logEntryUnKnown);
+        }
+
+    }
+
+}
